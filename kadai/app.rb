42ccac4f537becaaee5e6087e4d503704ab0cf1d
Kento Suzuki <38824465+Goose211@users.noreply.github.com>
2019-02-23 04:10:23 +0900

    Add files via upload

--- kadai/app.rb
+++ kadai/app.rb
@@ -38,8 +38,7 @@ end
 
 
 post '/signin' do
-　p params
-　user = User.find_by(name: params[:name])
+user = User.find_by(name: params[:name])
   if user && user.authenticate(params[:password])
     session[:user] = user.id
   end
@@ -65,8 +64,8 @@ end
 
 get '/search' do
   keyword = params[:keyword]
-  uri =URI("https://itunes.apple.com/search")
-  uri.query = URI.enable_www_form({ term: keyword,country: "US",media:"music",limit: 10})
+  uri = URI("https://itunes.apple.com/search")
+  uri.query = URI.encode_www_form({ term: keyword, country: "US", media: "music", limit: 10})
   res = Net::HTTP.get_response(uri)
   returned_json = JSON.parse(res.body)
   @musics = returned_json["results"]
