4e9054100cc3edc00b7d83a0400b2e17243cdcd2
Kento Suzuki <38824465+Goose211@users.noreply.github.com>
2019-02-22 23:14:47 +0900

    Add files via upload

--- kadai/app.rb
+++ kadai/app.rb
@@ -1,17 +1,28 @@
 require 'bundler/setup'
 Bundler.require
 require 'sinatra/reloader' if development?
+
+require 'sinatra/activerecord'
 require './models'
 
+#検索機能実装
+require 'open-uri'
+require 'net/http'
+require 'json'
+
+
 enable :sessions
+
+helpers do
+  def current_user
+    User.find_by(id: session[:user])
+  end
+end
 
 get '/' do
   erb :index
 end
 
-get '/signin' do
-  erb :sign_in
-end
 
 get '/signup' do
   erb :sign_up
@@ -21,21 +32,30 @@ get '/search' do
   erb :search
 end
 
+get '/home' do
+  erb :home
+end
+
+
 post '/signin' do
+　p params
 　user = User.find_by(name: params[:name])
   if user && user.authenticate(params[:password])
     session[:user] = user.id
   end
-  redirect '/search'
+  redirect '/'
 end
 
 post '/signup' do
-  @user = User.create(name:params[:name],password:params[:password],
-  password_confirmation:params[:password_confirmation],img: params[:file])
+  @user = User.create(name:params[:name],
+    password:params[:password],
+  password_confirmation:params[:password_confirmation],
+  img: params[:file])
+
   if @user.persisted?
     session[:user] = @user.id
   end
-  redirect '/search'
+  redirect '/'
 end
 
 get '/signout' do
@@ -45,5 +65,11 @@ end
 
 get '/search' do
   keyword = params[:keyword]
+  uri =URI("https://itunes.apple.com/search")
+  uri.query = URI.enable_www_form({ term: keyword,country: "US",media:"music",limit: 10})
+  res = Net::HTTP.get_response(uri)
+  returned_json = JSON.parse(res.body)
+  @musics = returned_json["results"]
+
   erb :search
 end
--- kadai/db/schema.rb
+++ kadai/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_21_163050) do
+ActiveRecord::Schema.define(version: 2019_02_22_130033) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,4 +19,5 @@ ActiveRecord::Schema.define(version: 2019_02_21_163050) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
